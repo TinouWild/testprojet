17b9166db410f95f821c7ddce8164d1088b940bb
Etienne Walch <tinouclt@gmail.com>
2018-11-27 10:46:54 +0100

    Formulaire de connexion etc

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login()
+    {
+        return $this->render('security/index.html.twig');
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout() {}
+}
